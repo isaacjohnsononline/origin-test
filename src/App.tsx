b43ec1f3580f12f6e75c2d4fd5519d3697aeba68
Isaac Johnson <isaac@junklabgames.com>
2021-01-11 13:39:29 +1000

    feat(table): basic material-ui table boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <Container fixed>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Phone</TableCell>
+              <TableCell>Website</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableCell>test</TableCell>
+            <TableCell>test</TableCell>
+            <TableCell>test</TableCell>
+            <TableCell>test</TableCell>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
 
 export default App;
