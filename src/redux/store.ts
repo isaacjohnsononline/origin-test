5a81466701c56845eab6f595dd7889cf35b1c612
Isaac Johnson <isaac@junklabgames.com>
2021-01-11 13:35:58 +1000

    feat(redux): basic redux store set up

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,1 +1,14 @@
-// primary redux store
+import thunk from 'redux-thunk';
+
+import tableReducer from './reducers/table';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const rootReducer = combineReducers<IRootState, TRootActions>({
+  table: tableReducer,
+  // the potential for more reducers in the furture
+});
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
