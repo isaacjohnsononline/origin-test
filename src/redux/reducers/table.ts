9a9b4ed7f5de1582e5f46c84b4d2fb793f004c1f
Isaac Johnson <isaac@junklabgames.com>
2021-01-11 13:32:39 +1000

    feat(redux): reducer boilerplate

--- src/redux/reducers/table.ts
+++ src/redux/reducers/table.ts
@@ -1,1 +1,31 @@
-// primary table reducer
+const initialState: ITableState = {
+  data: [],
+  loading: false,
+  error: '',
+};
+
+export default function tableReducer(state: ITableState = initialState, action: TTableActions): ITableState {
+  switch (action.type) {
+    case 'EDIT_DATA':
+      return {
+        ...state,
+      };
+    case 'SET_DATA':
+      return {
+        ...state,
+      };
+    case 'SET_LOADING':
+      return {
+        ...state,
+        loading: action.value,
+      };
+    case 'SET_ERROR':
+      return {
+        ...state,
+        error: action.value,
+      };
+
+    default:
+      return state;
+  }
+}
